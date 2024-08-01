2359faea87c489bd3e3defd076e69ba22ba9d077
The Onion Ninja <kay.sfwgithub@cutefuzzy.net>
2024-08-01 14:15:07 -0500

    merge Custom NPC Gifts

--- python/newcontexttags.py
+++ python/newcontexttags.py
@@ -4,8 +4,11 @@ Json is used by convertja.py
 """
 import csv
 import json
+import pyjson5
+import pprint
 
 outDict = {}
+ctDict = {}
 with open("New Context Tags.csv", newline='\n') as csvfile:
     thisreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in thisreader:
@@ -20,8 +23,29 @@ with open("New Context Tags.csv", newline='\n') as csvfile:
                 multiTags = tag.replace("\n", "").split(", ")
                 # print(multiTags)
                 outDict[row[0]]["Tags"] += multiTags
+                for thisTag in multiTags:
+                    if thisTag not in ctDict:
+                        ctDict[thisTag] = [row[0]]
+                    else:
+                        ctDict[thisTag].append(row[0])
             elif tag:
                 outDict[row[0]]["Tags"].append(tag)
+                if tag not in ctDict:
+                    ctDict[tag] = [row[0]]
+                else:
+                    ctDict[tag].append(row[0])
 
-with open("NewContextTags.json", 'w', encoding='utf-8') as f:
-    json.dump(outDict, f, indent=4, ensure_ascii=False)
+# with open("NewContextTags.json", 'w', encoding='utf-8') as f:
+#     json.dump(outDict, f, indent=4, ensure_ascii=False)
+i18n = pyjson5.load(open("H:/Stardew Raffadax Update/Raffadax-Complete-Production/1.6 Files/[CP] Raffadax Test/i18n/default.json", encoding="utf-8"))
+with open("TagsForMatrix.csv", "w", newline='', encoding="utf-8") as f:
+    writer = csv.writer(f)
+    for k, v in ctDict.items():
+        newrow = [k]
+        for itemID in v:
+            splitVal = itemID.split("_", 1)[1]
+            i18nVal = splitVal + ".DisplayName"
+            if i18nVal in i18n:
+                itemName = i18n[i18nVal]
+                newrow.append(itemName)
+        writer.writerow(newrow)
